8b62139a3000595a4b86d89714f867097a75dc4b
marcopassarela <marcopassarela@hotmail.com>
2024-10-30 12:17:49 -0300

    one commit

--- script.js
+++ script.js
@@ -6,11 +6,27 @@ window.addEventListener("load", () => {
     const mainContent = document.getElementById("conteudo-principal");
     const loadingBar = document.querySelector(".loading-bar");
     const loadingPercentage = document.getElementById("loading-percentage");
-    const loadingMusic = document.getElementById("loading-music"); // Adicionando o elemento de áudio
+    const loadingMusic = document.getElementById("loading-music");
+    const backgroundVideo = document.getElementById("background-video");
     let percentage = 0;
 
-    // Inicia a música ao começar o carregamento
-    loadingMusic.play();
+    // Função para iniciar o carregamento
+    function startLoading() {
+        backgroundVideo.play(); // Toca o vídeo ao iniciar o carregamento
+        loadingMusic.play(); // Toca a música ao iniciar o carregamento
+        incrementPercentage(); // Inicia o incremento da porcentagem
+
+        const startButton = document.getElementById("start-loading");
+        startButton.classList.add("fade-out"); // Adiciona a classe para iniciar a transição
+
+        // Aguarda a transição de opacidade antes de esconder o botão
+        startButton.addEventListener("transitionend", () => {
+            startButton.style.display = "none"; // Esconde o botão após a transição
+        });
+    }
+
+    // Adiciona o evento de clique no botão para iniciar o carregamento
+    document.getElementById("start-loading").addEventListener("click", startLoading);
 
     function incrementPercentage() {
         if (percentage < 100) {
@@ -26,9 +42,10 @@ window.addEventListener("load", () => {
                 setTimeout(incrementPercentage, 60);
             }
         } else {
-            // Para a música quando o carregamento atingir 100%
+            // Para a música e o vídeo quando o carregamento atingir 100%
             loadingMusic.pause(); // Para a música
             loadingMusic.currentTime = 0; // Reinicia a música para o início
+            backgroundVideo.pause(); // Pausa o vídeo
 
             // Aplica o efeito de fade-out ao completar 100%
             loadingScreen.classList.add("fade-out");
@@ -40,7 +57,5 @@ window.addEventListener("load", () => {
             }, 1000); // 1000ms corresponde ao tempo da transição CSS
         }
     }
-
-    // Inicia o carregamento
-    incrementPercentage();
 });
+
