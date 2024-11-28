7b68f6f78c801c015634d49be08918b46f41f133
marcopassarela <marcopassarela@hotmail.com>
2024-11-28 15:04:16 -0300

    test cookies

--- scripts/essential.js
+++ scripts/essential.js
@@ -44,7 +44,7 @@ function loadGoogleTagManager() {
 
     // Adicionar o iframe do GTM (fallback para usuários com noscript)
     const noscript = document.createElement("noscript");
-    noscript.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TX48535W" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
+    noscript.innerHTML = <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TX48535W" height="0" width="0" style="display:none;visibility:hidden"></iframe>;
     document.body.appendChild(noscript);
 }
 
@@ -54,6 +54,14 @@ function loadEssentialScripts() {
     const essentialScript = document.createElement("script");
     essentialScript.src = "/scripts/essential.js"; // Substitua com o caminho do seu script essencial
     document.head.appendChild(essentialScript);
+}
+
+// Função para fechar o modal
+function closeModal() {
+    const popup = document.getElementById("popup");
+    if (popup) {
+        popup.style.display = "none";
+    }
 }
 
 // Função para aplicar a escolha de cookies
@@ -70,6 +78,7 @@ function applyCookiePreferences() {
         loadEssentialScripts(); // Carrega apenas scripts essenciais (sem rastreamento)
     } else if (cookieChoice === "denyAll") {
         console.log("Nenhum cookie será carregado.");
+        closeModal();
         // Não carrega nenhum script adicional, apenas os essenciais.
     } else if (cookieChoice === "acceptAll-cookies") {
         console.log("Carregar todos os cookies até o de terceiros.")
@@ -79,7 +88,8 @@ function applyCookiePreferences() {
         // Carregara os cookies todos
     } else if (cookieChoice === "denyAll-cookies") {
         console.log("Nenhum cookie será carregado.");
-        
+        closeModal();
+        // Não carrega nenhum script adicional, apenas os essenciais.
     }
 }
 
