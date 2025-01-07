5881e006cae341445a8215a8f62a96dbc6ccffc0
marcopassarela <marcopassarela@hotmail.com>
2025-01-07 10:36:48 -0300

    upgrade all pages

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -1,12 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Lógica para exibir o ticket ID da URL
     const params = new URLSearchParams(window.location.search);
     const ticketId = params.get('ticket_form_id');
-  
     const infoElement = document.getElementById("ticket-info");
+    
     if (ticketId) {
-      infoElement.textContent = `Este é o ticket ID: ${ticketId}`;
+        infoElement.textContent = `Este é o ticket ID: ${ticketId}`;
     } else {
-      infoElement.textContent = "Nenhum ticket ID foi fornecido.";
+        infoElement.textContent = "Nenhum ticket ID foi fornecido.";
     }
-  });
-  
+});
+
+// Redirecionamento para as páginas com base na opção selecionada
+document.getElementById("redirectSelect").addEventListener("change", function() {
+    var selectedValue = this.value;
+    if (selectedValue) {
+        window.location.href = selectedValue; // Redireciona para a URL da opção selecionada
+    }
+});
