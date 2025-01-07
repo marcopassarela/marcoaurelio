66523fcbfde10c8baffdb0999c9119c0c9de6853
marcopassarela <marcopassarela@hotmail.com>
2025-01-07 16:17:14 -0300

    upgrade all pages support

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -11,10 +11,36 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
-// Redirecionamento para as páginas com base na opção selecionada
-document.getElementById("redirectSelect").addEventListener("change", function() {
-    var selectedValue = this.value;
-    if (selectedValue) {
-        window.location.href = selectedValue; // Redireciona para a URL da opção selecionada
-    }
+const fileInput = document.getElementById('file-input');
+const uploadedFiles = document.querySelector('.uploaded-files');
+const fileLabel = document.querySelector('.file-label');
+
+fileLabel.addEventListener('click', () => {
+  fileInput.click();
 });
+
+fileInput.addEventListener('change', () => {
+  uploadedFiles.innerHTML = '';
+  for (let i = 0; i < fileInput.files.length; i++) {
+    const file = fileInput.files[i];
+    const fileElement = document.createElement('div');
+    fileElement.classList.add('file-item');
+
+    const filePreview = document.createElement('div');
+    filePreview.classList.add('file-preview');
+    if (file.type.startsWith('image/')) {
+      filePreview.style.backgroundImage = `url(${URL.createObjectURL(file)})`;
+    } else {
+      filePreview.textContent = '📄';
+    }
+    fileElement.appendChild(filePreview);
+
+    const fileName = document.createElement('div');
+    fileName.classList.add('file-name');
+    fileName.textContent = file.name;
+    fileElement.appendChild(fileName);
+
+    uploadedFiles.appendChild(fileElement);
+  }
+});
+
